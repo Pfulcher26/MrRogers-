476c22f265cd9fb389e0c8515e1812a63b49e130
Payne Fulcher <pfulcher26@gmail.com>
2020-06-12 11:43:27 -0700

    Adds map function to test the validity of range array created from user input

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,20 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     const userNumber = $("#input1").val();
-    let result = Array.from({length: userNumber}, (x,i) => i);
-    $("#output").text(result);
+    const range = Array.from({length: userNumber}, (x,i) => i);
+    const newRange = range.map(function(element) {
+      return element * 2;
+    });
+    $("#output").text(newRange);
   });
 });
+
+/*let doubledArray = [];
+    result.forEach(function(element) {
+    if element === 1 {
+    alert("you crazy")
+  }
+  newarray.push(element);
+
+
+
