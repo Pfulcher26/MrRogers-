4c1958ef71384545b1b6b5634d39e2675290cd19
Payne Fulcher <pfulcher26@gmail.com>
2020-06-12 13:55:18 -0700

    Adds if statement to map function to account for actions to be taken if numbers in the array are equal to 1, 2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,13 @@ $(document).ready(function() {
     const userNumber = $("#input1").val();
     const range = Array.from({length: userNumber}, (x,i) => i);
     const newRange = range.map(function(element) {
-      return element * 2;
+      if (element === 1) {
+        element.replace(/1/g, "beep!");
+      } else if (element === 2) {
+        alert("Ho!");
+      } else {
+        return element * 2
+      }
     });
     $("#output").text(newRange);
   });
